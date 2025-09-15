325f0fc5d452c59a03bb02398f7ef5f593187cef
Victor <vevigo@yandex.ru>
2025-09-16 02:11:47 +0300

    add boosters 2

--- src/main/java/com/creazione/space_learning/dto/WeightedResource.java
+++ src/main/java/com/creazione/space_learning/dto/WeightedResource.java
@@ -1,5 +1,7 @@
 package com.creazione.space_learning.dto;
 
+import com.creazione.space_learning.entities.Resource;
+
 public class WeightedResource {
     private Resource resource;
     private double weight;
